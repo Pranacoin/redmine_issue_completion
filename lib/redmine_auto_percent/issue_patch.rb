f9df40584ff45e06cb25e596fbfcf5c36c1e7535
Konstans Shootkeen <1991konstantin@gmail.com>
2017-09-02 08:55:23 +0300

    Update issue_patch.rb

--- lib/redmine_auto_percent/issue_patch.rb
+++ lib/redmine_auto_percent/issue_patch.rb
@@ -6,7 +6,7 @@ module RedmineAutoPercent
       base.send(:include, InstanceMethods)
 
       base.class_eval do
-        before_save :update_percent
+ #       before_save :update_percent
  		before_save :update_due_date
       end
     end
