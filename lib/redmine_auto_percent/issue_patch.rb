296ecbbc71e5583420085176258b086fab8716be
Konstans Shootkeen <1991konstantin@gmail.com>
2017-09-02 10:57:30 +0300

    Update issue_patch.rb

--- lib/redmine_auto_percent/issue_patch.rb
+++ lib/redmine_auto_percent/issue_patch.rb
@@ -16,9 +16,13 @@ module RedmineAutoPercent
   end
 
   module InstanceMethods
-#    def update_percent
-  #    self.done_ratio = 100 if self.status.is_closed?
-#    end
+def object_custom_field_set_value
+  self.custom_field_values.each do |field|
+    if ( ( field.custom_field.id == 2 ) && self.status.is_closed?)
+      field.value = 100
+    end
+  end
+end
  	def update_due_date
  	  if (!(self.done_ratio == self.status.default_done_ratio) && self.status.is_closed?)
 		self.due_date = Date.today
